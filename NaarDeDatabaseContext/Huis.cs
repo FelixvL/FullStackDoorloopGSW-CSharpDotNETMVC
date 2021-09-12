feaff224b5461d2be050051ec13996e59ffb0860
FelixvL <digitaleservice@gmail.com>
2021-09-12 23:10:07 +0200

    De databaseContext en het Model zijn toegevoegd

---
+++ NaarDeDatabaseContext/Huis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaarDeDatabaseContext
+{
+    public class Huis
+    {
+        public int Id { get; set; }
+        public string Adres { get; set; }
+        public int Huisnummer { get; set; }
+    }
+}
